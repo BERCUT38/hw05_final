db5b555b61cf09f9d9ee5cbfe6ab0620e484db24
Сергей <bazzz@list.ru>
2021-09-24 21:40:37 +0300

    123

--- yatube/core/context_processors/year.py
+++
@@ -1,8 +0,0 @@
-from django.utils import timezone as tz
-
-
-def year(request):
-    """Добавляет переменную с текущим годом."""
-    now = tz.localtime(tz.now())
-    year = int(now.strftime("%Y"))
-    return {'year': year, }
